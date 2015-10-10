e2c1faa159362dbfc855b3e6303234d731c1f0d7
Casper Olsen <casperthedeolsen@Caspers-iMac.local>
2015-10-10 13:57:03 +0200

    Esbjerg, Visitationzoner

--- zone.php
+++ zone.php
@@ -1,12 +1,23 @@
 <?
 $dir = 'zones/';
-$sdir = array_diff(scandir($dir), array('..', '.'));
-natsort($sdir);
+if(!isset($_GET['update_zone']))
+{
+	$sdir = array_diff(scandir($dir), array('..', '.'));
+	natsort($sdir);
+}
+else
+{
+	$sdir = array($_GET['update_zone']);
+}
+
 $features = array();
 foreach($sdir as $file){
    $data = file_get_contents($dir.$file, 0, null, null);
    $json = json_decode($data);
-   array_push($features, $json->features[0]);
+   if(isset($json->features[0])){
+   	 $json->features[0]->properties->filename = $file;
+     array_push($features, $json->features[0]);   
+   }
 }
 
 $json->features = $features;
